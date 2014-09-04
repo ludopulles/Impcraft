ebe2a40808771a81fd70cbac01c784c6d6b16a99
ludopulles <ludo.pulles@gmail.com>
2014-09-04 21:51:39 +0200

    poging om gitignores toe te voegen
    eclipse/.metadata/ moet niet meer in de commits komen, net als bin/

--- src/main/java/mod/impcraft/common/ModImpCraft.java
+++ src/main/java/mod/impcraft/common/ModImpCraft.java
@@ -3,12 +3,16 @@ package mod.impcraft.common;
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ModImpCraft.MODID, version = ModImpCraft.VERSION)
 public class ModImpCraft {
 	public static final String MODID = "impcraft", VERSION = "0.1";
+	
+	@Instance(MODID)
+	public static ModImpCraft instance;
 
 	public ModImpCraft() {
 
